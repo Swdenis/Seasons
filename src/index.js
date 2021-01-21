6ee9d1e3249c79fae9ffe36266dcb15e22f1ccc6
Swdenis <swdenis@mail.ru>
2021-01-21 19:06:37 +0100

    Refactored the code using lifecycle methods

--- src/index.js
+++ src/index.js
@@ -10,13 +10,12 @@ class App extends React.Component {
             errorMessage: '',
         };
 
+    }
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({lat: position.coords.latitude});
-            },
-            err => {
-                this.setState({ errorMessage : err.message});
-            }
+            position => this.setState({lat: position.coords.latitude}),
+            err => this.setState({ errorMessage : err.message})
         );
     }
 
